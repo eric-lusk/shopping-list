0393a57b10d4a2fac71607241dee4e8ab9626398
Eric Lusk <eric.m.lusk@outlook.com>
2018-06-27 10:43:12 -0400

    added relevant functions to source c file

--- ItemToPurchase.c
+++ ItemToPurchase.c
@@ -1,4 +1,16 @@
 // ItemToPurchase.c - Related function definitions
+#include "ItemToPurchase.h"
 #include <stdio.h>
 #include <string.h>
-#include "ItemToPurchase.h"
+
+void MakeItemBlank(ItemToPurchase *itemPtr) {
+  strcpy(itemPtr->itemName, "none");
+  itemPtr->itemPrice = 0;
+  itemPtr->itemQuantity = 0;
+}
+
+void PrintItemCost (ItemToPurchase *itemPtr) {
+  int totalamount = (itemPtr->itemQuantity) * (itemPtr->itemPrice);
+  printf("%s %d @ $%d = $%d\n",itemPtr->itemName, itemPtr->itemQuantity,
+  itemPtr->itemPrice, totalamount);
+}
