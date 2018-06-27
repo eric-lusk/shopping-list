5b81cf08f9c73e155604b94920b4a79580be7697
Eric Lusk <eric.m.lusk@outlook.com>
2018-06-27 09:32:52 -0400

    Added comments and a README file for further clarification

--- ItemToPurchase.c
+++ ItemToPurchase.c
@@ -1,5 +1,4 @@
+// ItemToPurchase.c - Related function definitions
 #include <stdio.h>
 #include <string.h>
 #include "ItemToPurchase.h"
-
-
--- ItemToPurchase.h
+++ ItemToPurchase.h
@@ -1,3 +1,20 @@
+/**
+* ItemToPurchase.h - Struct definition and related function declarations
+*
+* Build the ItemToPurchase struct with the following specifications:
+*
+* Data members (3 pts)
+* char itemName [ ]
+* int itemPrice
+* int itemQuantity
+* Related functions
+* MakeItemBlank() (2 pts)
+* Has a pointer to an ItemToPurchase parameter.
+* Sets item's name = "none", item's price = 0, item's quantity = 0
+* PrintItemCost()
+* Has an ItemToPurchase parameter.
+*/
+
 typedef struct ItemToPurchase_struct {
   char *itemName;
   int itemPrice;
